503e53c12ec301d1474a70986ffc666dc1889c85
eLtMosen <koennecke@mosushi.de>
2022-10-07 22:48:15 +0200

    Extend install.sh with 5 new ppd files

--- install.sh
+++ install.sh
@@ -19,11 +19,16 @@ then
     fi
 
     sudo cp Kyocera_FS-1040GDI.ppd /usr/share/cups/model/Kyocera/Kyocera_FS-1040GDI.ppd
+    sudo cp Kyocera_FS-1041GDI.ppd /usr/share/cups/model/Kyocera/Kyocera_FS-1041GDI.ppd
     sudo cp Kyocera_FS-1020MFPGDI.ppd /usr/share/cups/model/Kyocera/Kyocera_FS-1020MFPGDI.ppd
     sudo cp Kyocera_FS-1025MFPGDI.ppd /usr/share/cups/model/Kyocera/Kyocera_FS-1025MFPGDI.ppd
     sudo cp Kyocera_FS-1120MFPGDI.ppd /usr/share/cups/model/Kyocera/Kyocera_FS-1120MFPGDI.ppd
     sudo cp Kyocera_FS-1125MFPGDI.ppd /usr/share/cups/model/Kyocera/Kyocera_FS-1125MFPGDI.ppd
-    sudo cp Kyocera_FS-1060DNGDI.ppd /usr/share/cups/model/Kyocera/Kyocera_FS-1060DNGDI.ppd 
+    sudo cp Kyocera_FS-1220MFPGDI.ppd /usr/share/cups/model/Kyocera/Kyocera_FS-1220MFPGDI.ppd
+    sudo cp Kyocera_FS-1320MFPGDI.ppd /usr/share/cups/model/Kyocera/Kyocera_FS-1320MFPGDI.ppd
+    sudo cp Kyocera_FS-1325MFPGDI.ppd /usr/share/cups/model/Kyocera/Kyocera_FS-1325MFPGDI.ppd
+    sudo cp Kyocera_FS-1060DNGDI.ppd /usr/share/cups/model/Kyocera/Kyocera_FS-1060DNGDI.ppd
+    sudo cp Kyocera_FS-1061DNGDI.ppd /usr/share/cups/model/Kyocera/Kyocera_FS-1061DNGDI.ppd
     sudo cp bin/rastertokpsl-re /usr/lib/cups/filter/rastertokpsl
     
     if [ -f /usr/lib/cups/filter/rastertokpsl ] && [ -f /usr/share/cups/model/Kyocera/Kyocera_FS-1040GDI.ppd ] && [ -f /usr/share/cups/model/Kyocera/Kyocera_FS-1060DNGDI.ppd ] && [ -f /usr/share/cups/model/Kyocera/Kyocera_FS-1020MFPGDI.ppd ] && [ -f /usr/share/cups/model/Kyocera/Kyocera_FS-1025MFPGDI.ppd ] && [ -f /usr/share/cups/model/Kyocera/Kyocera_FS-1120MFPGDI.ppd ] && [ -f /usr/share/cups/model/Kyocera/Kyocera_FS-1125MFPGDI.ppd ]
